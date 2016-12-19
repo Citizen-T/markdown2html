c0f836d4cdf43aa7a79a1fbdfe0e231c61fb557a
Michael Triantafelow <MTriantafelow@gmail.com>
2016-12-18 22:28:24 -0500

    extracted method for converting directory to html

--- main.dart
+++ main.dart
@@ -2,8 +2,12 @@ import 'dart:io';
 
 import 'package:markdown/markdown.dart';
 
-main(args) async {
+main(args) {
     var dir = new Directory(args.first);
+    toHtml(dir);
+}
+
+toHtml(dir) async {
     Stream<FileSystemEntity> entities = dir.list(recursive: false, followLinks: false);
     await for (FileSystemEntity entity in entities) {
         var contents = await new File(entity.path).readAsString();
