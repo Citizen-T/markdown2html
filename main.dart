3c44aa24ce27e51a9ea4696c81ab26a663da93ce
Michael Triantafelow <MTriantafelow@gmail.com>
2016-12-18 23:20:30 -0500

    fixed bug that wrote the HTML files to the wrong place

--- main.dart
+++ main.dart
@@ -4,22 +4,27 @@ import 'package:markdown/markdown.dart';
 
 main(args) {
     var dir = new Directory(args.first);
+    print('args: ${args.first}');
     toHtml(dir);
 }
 
 toHtml(dir) async {
     Stream<FileSystemEntity> entities = dir.list(recursive: false, followLinks: false);
     await for (FileSystemEntity entity in entities) {
+        print('checking: ${entity.path}');
         var path = entity.path;
         var type = await FileSystemEntity.type(entity.path);
         switch (type) {
             case FileSystemEntityType.DIRECTORY:
+                print('is dir: ${entity.path}');
                 toHtml(new Directory(entity.path));
                 break;
             case FileSystemEntityType.FILE:
+                print('is file: ${entity.path}');
                 if (!path.endsWith('.md'))
                     break;
-                var htmlFile = await new File('${path.substring(path.length-3)}.html').create();
+                print('is markdown: ${entity.path}');
+                var htmlFile = await new File('${path.substring(0, path.length-3)}.html').create();
                 var contents = await new File(entity.path).readAsString();
                 var html = markdownToHtml(contents);
                 await htmlFile.writeAsString(html);
